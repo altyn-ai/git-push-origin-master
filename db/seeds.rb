619e2b5db2e3d4e14213e5ed25c1c7c91f1097c0
Altynai Adabaeva <altynai.a@posteo.de>
2021-02-19 15:15:24 +0100

    reinstalled bundler

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+data = open(url).read
+ingredients = JSON.parse(data)['drinks']
+# puts ingredients
+ingredients.each do |value|
+  Ingredient.create(name: value['strIngredient1'])
+end
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+# Ingredient.create(name: "ginger ale")
+# Ingredient.create(name: "tequila")
+# Ingredient.create(name: "tonic")
